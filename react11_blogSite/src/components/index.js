e4a299426f6a9f5885fc1670e3345884d5e59fbf
Jitendra Singh Bisht <jitendrasinghbisht1404m@gmail.com>
2024-03-20 22:23:49 +0530

    post form and rte

--- react11_blogSite/src/components/index.js
+++ react11_blogSite/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from './footer/Footer'
 import Container from './container/Container'
 import LogoutBtn from './header/LogoutBtn'
 import Logo from './Logo'
+import RTE from './RTE'
 
-export {Header, Footer, Logo, Container, LogoutBtn}
+export {Header, Footer, Logo, Container, LogoutBtn, RTE}
